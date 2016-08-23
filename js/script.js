636db6ccd68377f19892f7162dbf65ef4f6f8790
AJtwiss01 <arthurjtwiss@Arthurs-MacBook-Pro.local>
2016-08-22 18:03:49 -0700

    added more info my json

--- js/script.js
+++ js/script.js
@@ -1,110 +1,84 @@
+var quotes = [{
+    "Rank": 1,
+    "Quotation": "Frankly, my dear, I don't give a damn.",
+    "Character": "Rhett Butler",
+    "Actor": "Clark Gable",
+    "Film": "Gone with the Wind",
+    "Year": 1939
+}, {
+    "Rank": 2,
+    "Quotation": "I'm gonna make him an offer he can't refuse.",
+    "Character": "Vito Corleone",
+    "Actor": "Marlon Brando",
+    "Film": "The Godfather",
+    "Year": 1972
+}, {
+    "Rank": 4,
+    "Quotation": "You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.",
+    "Character": "Dorothy Gale",
+    "Actor": "Judy Garland",
+    "Film": "The Wizard of Oz",
+    "Year": 1939
+}, {
+    "Rank": 5,
+    "Quotation": "Here's looking at you, kid.",
+    "Character": "Rick Blaine",
+    "Actor": "Humphrey Bogart",
+    "Film": "Casablanca",
+    "Year": 1942
+}, {
+    "Rank": 6,
+    "Quotation": "Go ahead, make my day.",
+    "Character": "Harry Callahan",
+    "Actor": "Clint Eastwood",
+    "Film": "Sudden Impact",
+    "Year": 1983
+}, {
+    "Rank": 8,
+    "Quotation": "All right, Mr. DeMille, I'm ready for my close-up.",
+    "Character": "Han Solo",
+    "Actor": "Harrison Ford",
+    "Film": "Star Wars",
+    "Year": 1977
+}, {
+    "Rank": 9,
+    "Quotation": "Fasten your seatbelts. It's going to be a bumpy night. ",
+    "Character": "Margo Channing",
+    "Actor": "Bette Davis",
+    "Film": "All About Eve",
+    "Year": 1950
+},{
+    "Rank": 10,
+    "Quotation": "You talkin' to me?",
+    "Character": "Travis Bickle",
+    "Actor": "Robert De Niro",
+    "Film": "Taxi Driver",
+    "Year": 1976
+}]
 
-var quotes = [
-  {
-    "#": 1,
-    "QUOTE": "Frankly, my dear, I don't give a damn.",
-    "MOVIE": "GONE WITH THE WIND",
-    "YEAR": 1939,
-    "ACTOR": "Clark Gable's",
-    "CHARACTER": "Rhett Butler"
-  },
-  {
-    "#": 2,
-    "QUOTE": "I'm gonna make him an offer he can't refuse.",
-    "MOVIE": "THE GODFATHER",
-    "YEAR": 1972,
-    "ACTOR": "Marlon Brando",
-    "CHARACTER": "Don Corleone"
-  },
-  {
-    "#": 3,
-    "QUOTE": "You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.",
-    "MOVIE": "ON THE WATERFRONT",
-    "YEAR": 1954,
-    "ACTOR": "Marlon Brando",
-    "CHARACTER": "Don Corleone"
-  },
-  {
-    "#": 4,
-    "QUOTE": "Toto, I've a feeling we're not in Kansas anymore.",
-    "MOVIE": "THE WIZARD OF OZ",
-    "YEAR": 1939,
-    "ACTOR": "Marlon Brando",
-    "CHARACTER": "Don Corleone"
-  },
-  {
-    "#": 5,
-    "QUOTE": "Here's looking at you, kid.",
-    "MOVIE": "CASABLANCA",
-    "YEAR": 1942,
-    "ACTOR": "Marlon Brando",
-    "CHARACTER": "Don Corleone"
-  },
-  {
-    "#": 6,
-    "QUOTE": "Go ahead, make my day.",
-    "MOVIE": "SUDDEN IMPACT",
-    "YEAR": 1983,
-    "ACTOR": "Marlon Brando",
-    "CHARACTER": "Don Corleone"
-  },
-  {
-    "#": 7,
-    "QUOTE": "All right, Mr. DeMille, I'm ready for my close-up.",
-    "MOVIE": "SUNSET BLVD.",
-    "YEAR": 1950,
-    "ACTOR": "Marlon Brando",
-    "CHARACTER": "Don Corleone"
-  },
-  {
-    "#": 8,
-    "QUOTE": "May the Force be with you.",
-    "MOVIE": "STAR WARS",
-    "YEAR": 1977,
-    "ACTOR": "Marlon Brando",
-    "CHARACTER": "Don Corleone"
-  },
-  {
-    "#": 9,
-    "QUOTE": "Fasten your seatbelts. It's going to be a bumpy night.",
-    "MOVIE": "ALL ABOUT EVE",
-    "YEAR": 1950,
-   "ACTOR": "Marlon Brando",
-    "CHARACTER": "Don Corleone"
-  },
-  {
-    "#": 10,
-    "QUOTE": "You talking to me?",
-    "MOVIE": "TAXI DRIVER",
-    "YEAR": 1976,
-    "ACTOR": "Marlon Brando",
-    "CHARACTER": "Don Corleone"
-  }
-];
-
-function getRandomQuote(random){
-var random = quotes[Math.floor(Math.random() * quotes.length)];
+function getRandomQuote(random) {
+    var random = quotes[Math.floor(Math.random() * quotes.length)];
     console.log(random);
     console.log(random.QUOTE);
     console.log(random.MOVIE);
     console.log(random.YEAR);
- 
+
 
     return random;
 
 
 };
-function printQuote(addQoute){
-	console.log(getRandomQuote());
-	var logic=getRandomQuote();
-	var quoteOutput = "<p class='quote'>"+ logic.QUOTE+ "</p> <p class='source'>"+ logic.ACTOR+"<span class='citation'>"+ logic.MOVIE+"</span> <span class='year'>"+ logic.YEAR +"</span> </p>";
-	console.log(quoteOutput);
-	var addQoute = document.getElementById('quote-box').innerHTML = quoteOutput;
-	return addQoute;
+
+function printQuote(quoteOutput) {
+    console.log(getRandomQuote());
+    var logic = getRandomQuote();
+    var quoteOutput = "<p class='quote'>" + logic.Quotation + "</p> <p class='source'>" + logic.Actor + "<span class='citation'>" + logic.Film + "</span> <span class='year'>" + logic.Year + "</span> </p>";
+    console.log(quoteOutput);
+    var addQoute = document.getElementById('quote-box').innerHTML = quoteOutput;
+    return addQoute;
 }
 
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
